f544ff23b9caab31e4f90c52c835638c24c4fee5
EC2 Default User <ec2-user@ip-172-31-20-23.ec2.internal>
2022-08-27 10:47:05 +0000

    [add]user_imageの追加

--- app/models/user_image.rb
+++
@@ -1,4 +0,0 @@
-class UserImage < ApplicationRecord
-  has_one_attached :image
-  belongs_to :user
-end
--- db/migrate/20220827063746_create_user_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserImages < ActiveRecord::Migration[6.1]
-  def change
-    create_table :user_images do |t|
-      t.integer :user_id
-
-      t.timestamps
-    end
-  end
-end
