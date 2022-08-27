5744b9faa3b84f2162cb50362cb98c351d94195a
EC2 Default User <ec2-user@ip-172-31-20-23.ec2.internal>
2022-08-27 20:01:19 +0000

    [add]様々なページに各ページのリンクを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
+  resources :books, only: [:new, :index, :show, :edit, :update, :create, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
   get 'homes/about' => "homes#about", as: "about"
